839378f2a5305017b3212746286401db96f90771
ArondightWhite <jekpadtm@gmail.com>
2021-01-19 16:05:17 +0300

    Done programm user.py

---
+++ user.py
@@ -0,0 +1,7 @@
+print("Hello")
+
+Name = input("What is your name? \n")
+Age = input("How old are you? \n")
+Place = input("Where are you live? \n")
+
+print("\nThis is {0} \n(S)he is {1} years old\n(S)he live in {2}".format(Name, Age, Place))
