1c5e9db70a539dca7f39b26171b7f845f331b284
ArondightWhite <jekpadtm@gmail.com>
2021-01-19 16:34:34 +0300

    Add & done programm numbers.py

---
+++ numbers.py
@@ -0,0 +1,10 @@
+print("Write 4 numbers")
+
+Num1 = int(input("1st num: "))
+Num2 = int(input("2st num: "))
+Num3 = int(input("3st num: "))
+Num4 = int(input("4st num: "))
+
+NumResult = (Num1 + Num2) / (Num3 + Num4)
+
+print("%.2f" % NumResult)
